db98afbe69567150cee5a5d1cd0a6c31c74ae3c7
graceco22 <graceco921@gmail.com>
2024-06-27 10:38:32 -0700

    added island + fixed loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import Island from "../models/Island";
+import Loader from "../components/Loader";
 
 /*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
 POPUP
 </div> */
 
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas
@@ -14,12 +33,16 @@ const Home = () => {
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
-          <pointLight position={[10, 5, 10]} intensity={2} />
-          <spotLight
-            position={[0, 50, 10]}
-            angle={0.15}
-            penumbra={1}
-            intensity={2}
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
           />
         </Suspense>
       </Canvas>
