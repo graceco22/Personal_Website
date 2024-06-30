e812f8bece5ae7a934b3f955f7591c0cba7b3c97
graceco22 <graceco921@gmail.com>
2024-06-29 21:07:35 -0700

    finished popups

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,10 @@ import { useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Island from "../models/Island";
 import Loader from "../components/Loader";
+import HomeInfo from "../components/HomeInfo";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
-
-/*<div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-POPUP
-</div> */
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
@@ -49,6 +46,9 @@ const Home = () => {
 
   return (
     <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage} />}
+      </div>
       <Canvas
         className={`w-full h-screen bg-transparent ${
           isRotating ? "cursor-grabbing" : "cursor-grab"
