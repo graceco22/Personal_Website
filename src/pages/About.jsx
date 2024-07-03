3676ea13d61b69a90bcb91ae4a23425853d7fc76
graceco22 <graceco921@gmail.com>
2024-07-02 18:22:18 -0700

    changed about background

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,96 +9,98 @@ import CTA from "../components/CTA";
 
 const About = () => {
   return (
-    <section className="max-container">
-      <h1 className="head-text">
-        Hello, I'm{" "}
-        <span class="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-green-300 font-semibold drop-shadow">
-          Grace
-        </span>
-      </h1>
+    <div className="bg-gray-100">
+      <section className="max-container">
+        <h1 className="head-text">
+          Hello, I'm{" "}
+          <span class="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-green-300 font-semibold drop-shadow">
+            Grace
+          </span>
+        </h1>
 
-      <div className="mt-5 flex flex-col gap-3 text-slate-500">
-        <p>
-          Computer Science and Master of Management student at The University of
-          British Columbia.
-        </p>
-      </div>
-
-      <div className="py-10 flex flex-col">
-        <h3 className="subhead-text">My Skills</h3>
-        <div className="mt-16 flex flex-wrap gap-12">
-          {skills.map((skill) => (
-            <div className="block-container w-20 h-20" key={skill.name}>
-              <div className="btn-back rounded-xl" />
-              <div className="btn-front rounded-xl flex justify-center items-center">
-                <img
-                  src={skill.imageUrl}
-                  alt={skill.name}
-                  className="w-1/2 h-1/2 object-contain"
-                />
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="py-16">
-        <h3 className="subhead-text">Work Experience</h3>
         <div className="mt-5 flex flex-col gap-3 text-slate-500">
-          <p></p>
+          <p>
+            Computer Science and Master of Management student at The University
+            of British Columbia.
+          </p>
         </div>
 
-        <div className="mt-12 flex">
-          <VerticalTimeline>
-            {experiences.map((experience, index) => (
-              <VerticalTimelineElement
-                key={experience.company_name}
-                date={experience.date}
-                icon={
-                  <div className="flex justify-center items-center w-full h-full">
-                    <img
-                      src={experience.icon}
-                      alt={experience.company_name}
-                      className="w-[60%] h-[60%] object-contain"
-                    />
-                  </div>
-                }
-                iconStyle={{ background: experience.iconBg }}
-                contentStyle={{
-                  borderBottom: "8px",
-                  borderStyle: "solid",
-                  borderBottomColor: experience.iconBg,
-                  boxShadow: "none",
-                }}>
-                <div>
-                  <h3 className="text-black text-xl font-poppins font-semibold">
-                    {experience.title}
-                  </h3>
-                  <p
-                    className="text-black-500 font-medium font-base"
-                    style={{ margin: 0 }}>
-                    {experience.company_name}
-                  </p>
+        <div className="py-10 flex flex-col">
+          <h3 className="subhead-text">My Skills</h3>
+          <div className="mt-16 flex flex-wrap gap-12">
+            {skills.map((skill) => (
+              <div className="block-container w-20 h-20" key={skill.name}>
+                <div className="btn-back rounded-xl" />
+                <div className="btn-front rounded-xl flex justify-center items-center">
+                  <img
+                    src={skill.imageUrl}
+                    alt={skill.name}
+                    className="w-1/2 h-1/2 object-contain"
+                  />
                 </div>
-                <ul className="my-5 list-disc ml-5 space-y-2">
-                  {experience.points.map((point, index) => (
-                    <li
-                      key={`experience-point-${index}`}
-                      className="text-black-500/50 font-normal pl-1 text-sm">
-                      {point}
-                    </li>
-                  ))}
-                </ul>
-              </VerticalTimelineElement>
+              </div>
             ))}
-          </VerticalTimeline>
+          </div>
         </div>
-      </div>
 
-      <hr className="border-slate-200" />
+        <div className="py-16">
+          <h3 className="subhead-text">Experience</h3>
+          <div className="mt-5 flex flex-col gap-3 text-slate-500">
+            <p></p>
+          </div>
 
-      <CTA />
-    </section>
+          <div className="mt-12 flex">
+            <VerticalTimeline>
+              {experiences.map((experience, index) => (
+                <VerticalTimelineElement
+                  key={experience.company_name}
+                  date={experience.date}
+                  icon={
+                    <div className="flex justify-center items-center w-full h-full">
+                      <img
+                        src={experience.icon}
+                        alt={experience.company_name}
+                        className="w-[60%] h-[60%] object-contain"
+                      />
+                    </div>
+                  }
+                  iconStyle={{ background: experience.iconBg }}
+                  contentStyle={{
+                    borderBottom: "8px",
+                    borderStyle: "solid",
+                    borderBottomColor: experience.iconBg,
+                    boxShadow: "none",
+                  }}>
+                  <div>
+                    <h3 className="text-black text-xl font-poppins font-semibold">
+                      {experience.title}
+                    </h3>
+                    <p
+                      className="text-black-500 font-medium font-base"
+                      style={{ margin: 0 }}>
+                      {experience.company_name}
+                    </p>
+                  </div>
+                  <ul className="my-5 list-disc ml-5 space-y-2">
+                    {experience.points.map((point, index) => (
+                      <li
+                        key={`experience-point-${index}`}
+                        className="text-black-500/50 font-normal pl-1 text-sm">
+                        {point}
+                      </li>
+                    ))}
+                  </ul>
+                </VerticalTimelineElement>
+              ))}
+            </VerticalTimeline>
+          </div>
+        </div>
+
+        <hr className="border-slate-200" />
+
+        <CTA />
+      </section>
+    </div>
   );
 };
 
